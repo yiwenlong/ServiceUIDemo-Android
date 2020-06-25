13a7002ef6bd91ad1778e2ebf47b1eeb4247c2d3
yiwenlong <wlong.yi@gmail.com>
2020-06-25 19:58:13 +0800

    Fix bug: notification error.

--- service/src/main/java/com/yiwenlong/service/ServerManagerService.java
+++ service/src/main/java/com/yiwenlong/service/ServerManagerService.java
@@ -4,12 +4,13 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class ServerManagerService extends Service {
 
@@ -20,14 +21,23 @@ public class ServerManagerService extends Service {
     public void onCreate() {
         Log.i("ServerManagerService", "onCreate");
         super.onCreate();
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_HIGH);
 
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.createNotificationChannel(channel);
+        NotificationManagerCompat.from(this)
+                .createNotificationChannel(
+                        new NotificationChannel(
+                                CHANNEL_ID,
+                                "a demo channel",
+                                NotificationManager.IMPORTANCE_DEFAULT
+                        )
+                );
 
-        Notification notification = new Notification.Builder(getApplicationContext(),CHANNEL_ID).build();
-        startForeground(1, notification);
+        Notification notification = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID)
+                .setContentText("这是一个 Android 下运行 Golang server 的 App")
+                .setContentTitle("ServiceUI Shell")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setGroup(CHANNEL_ID)
+                .build();
+        startForeground(256, notification);
     }
 
     @Nullable
